1431dd03b85528162185e8ea0b3e6130fe0aace7
Guilherme Poppi <guilhermepoppilm@gmail.com>
2025-02-12 02:25:11 -0300

    add elementos na navegation

--- src/app/components/Navegation/index.tsx
+++ src/app/components/Navegation/index.tsx
@@ -2,7 +2,8 @@
 import { useTheme } from "next-themes"
 import { useEffect, useState } from "react"
 import Link from "next/link"
-import { Moon, Sun } from "lucide-react"
+import { Moon, Sun, Mail } from "lucide-react"
+
 
 export default function Navigation() {
   const { theme, setTheme } = useTheme()
@@ -18,50 +19,57 @@ export default function Navigation() {
 
   return (
     <nav
-      className={`p-4 flex gap-48 items-center transition-all duration-500 
-      ${
-        theme === "dark"
-          ? "bg-gradient-to-r from-neutral-900 via-zinc-800 text-white"
-          : "bg-gradient-to-r from-blue-50 via-white to-blue-50 text-gray-800"
-      }`}
+    className={`p-4 flex gap-48 items-center transition-all duration-500 
+        ${theme === "dark" 
+          ? "bg-gradient-to-r from-neutral-900 via-zinc-800 text-white" 
+          : "bg-gradient-to-r from-white via-gray-100 to-white text-gray-800 shadow-lg"
+        }`}
+      
+
     >
       <div className="text-xl font-bold flex justify-end w-96 items-center">
         <Link href="">InterCompnet</Link>
       </div>
       <div className="flex gap-5 justify-center font-normal text-sm items-center">
-        <Link className="hover:text-gray-900 dark:hover:text-gray-200 transition-all duration-300" href="">
+        <Link className="hover:text-neutral-400 dark:hover:text-zinc-400 transition-all duration- " href="">
           Sobre Nós
         </Link>
-        <Link className="hover:text-gray-900 dark:hover:text-gray-200 transition-all duration-300" href="">
+        <Link className="hover:text-neutral-400 dark:hover:text-zinc-400 transition-all duration- " href="">
           Como Funciona
         </Link>
-        <Link className="hover:text-gray-900 dark:hover:text-gray-200 transition-all duration-300" href="">
+        <Link className="hover:text-neutral-400 dark:hover:text-zinc-400 transition-all duration- " href="">
           Serviços
         </Link>
-        <Link className="hover:text-gray-900 dark:hover:text-gray-200 transition-all duration-300" href="">
+        <Link className="hover:text-neutral-400 dark:hover:text-zinc-400 transition-all duration- " href="">
           Marcas
         </Link>
-        <Link className="hover:text-gray-900 dark:hover:text-gray-200 transition-all duration-300" href="">
+        <Link className="hover:text-neutral-400 dark:hover:text-zinc-400 transition-all duration- " href="">
           Galeria
         </Link>
-        <Link className="hover:text-gray-900 dark:hover:text-gray-200 transition-all duration-300" href="">
+        <Link className="hover:text-neutral-400 dark:hover:text-zinc-400 transition-all duration- " href="">
           Contato
         </Link>
       </div>
 
-      {/* Theme toggle button */}
-      <div>
+      
+
+      <div className="flex gap-6 text-center items-center justify-center">
         <button
           onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-          className={`p-2 rounded-full transition-all duration-300
+          className={`p-2 rounded-full transition-all duration-200
             ${
               theme === "dark"
-                ? "bg-gray-700 text-white hover:bg-gray-600"
-                : "bg-blue-100 text-gray-800 hover:bg-blue-200"
+                ? " border-2 text-white hover:bg-neutral-800"
+                : "border-2 text-gray-800 hover:bg-zinc-200"
             }`}
         >
           {theme === "light" ? <Moon size={20} /> : <Sun size={20} />}
+
         </button>
+        
+        <a className="text-sm pl-4 pr-6 py-2 border-[1.5px] rounded-md text-left flex items-center justify-center gap-3 hover:bg-zinc-200 dark:hover:bg-zinc-800 transition-colors duration-200" href="#"><Mail size={20}/>Email</a>
+        
+
       </div>
     </nav>
   )
