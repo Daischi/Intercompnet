89fca155282b3ef83e6d28ed6fee2f6f3fcdc741
Guilherme Poppi <guilhermepoppilm@gmail.com>
2025-02-21 15:00:13 -0300

    add outras abas

--- src/components/Servicespage/index.tsx
+++ src/components/Servicespage/index.tsx
@@ -7,7 +7,7 @@ export default function ServicesTabs() {
     <div>
       <div>
         <h1 className="flex justify-center items-center mb-4 text-3xl font-bold">Nossos Serviços</h1>
-        <p className="flex justify-center items-center mb-4">Oferecemos uma ampla gama de serviços técnicos especializados</p>
+        <p className="flex justify-center items-center mb-10">Oferecemos uma ampla gama de serviços técnicos especializados</p>
       </div>
 
       <Tabs defaultValue="computadores" className="w-full max-w-4xl mx-auto">
@@ -45,25 +45,43 @@ export default function ServicesTabs() {
               </CardHeader>
               <CardContent>Recuperação de arquivos perdidos.</CardContent>
             </Card>
+          </div>
+        </TabsContent>
+
+        <TabsContent value="impressoras">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-4">
+            <Card>
+              <CardHeader>
+                <Cpu className="w-6 h-6" />
+                <CardTitle>Manutenção de PCs</CardTitle>
+              </CardHeader>
+              <CardContent>
+                Serviços completos para desktops e notebooks.
+              </CardContent>
+            </Card>
 
             <Card>
               <CardHeader>
-                <HardDrive className="w-6 h-6" />
-                <CardTitle>Novos</CardTitle>
+                <Download className="w-6 h-6" />
+                <CardTitle>Instalação de Software</CardTitle>
               </CardHeader>
-              <CardContent>Recuperação de arquivos perdidos.</CardContent>
+              <CardContent>Sistemas operacionais e programas.</CardContent>
             </Card>
 
             <Card>
               <CardHeader>
                 <HardDrive className="w-6 h-6" />
-                <CardTitle>Novos</CardTitle>
+                <CardTitle>Recuperação de Dados</CardTitle>
               </CardHeader>
               <CardContent>Recuperação de arquivos perdidos.</CardContent>
             </Card>
           </div>
         </TabsContent>
+
+
       </Tabs>
+
+      
     </div>
   );
 }
