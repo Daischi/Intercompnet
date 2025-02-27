a60a9309e1b5e876bd60e7bb5251f1e595d63a74
Guilherme Poppi <guilhermepoppilm@gmail.com>
2025-02-26 22:31:21 -0300

    continuando projeto

--- src/components/guiafunciona/index.tsx
+++ src/components/guiafunciona/index.tsx
@@ -1,15 +1,18 @@
 export default function guiafunciona() {
   return (
+    <div>
+      <div>
+        <div>
 
-<div>
+        
+        <h3>
+        Processo Simplificado
+        </h3>
+         <h1>Como Funciona?</h1>   
+         <h2>Conheça nosso processo de atendimento passo a passo</h2>
 
-
-
-
-
-
-    
-
-</div>
-
-  )}
+         </div>
+      </div>
+    </div>
+  );
+}
