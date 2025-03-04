7cbf7cbe43669f18472cf70f157aab2618ca727f
Guilherme Poppi <guilhermepoppilm@gmail.com>
2025-03-04 01:16:01 -0300

    add check

--- src/components/Sobrenos/index.tsx
+++ src/components/Sobrenos/index.tsx
@@ -1,4 +1,5 @@
 import banner from "@/utils/banner.webp";
+import { CircleCheck } from "lucide-react";
 import Image from "next/image"; 
 
 export default function Sobrenos() {
@@ -19,8 +20,8 @@ export default function Sobrenos() {
 
   {/* Texto */}
   <div className="flex-1">
-    <h1 className="text-3xl font-bold mb-8">Sobre Nós</h1>
-    <p className="mb-8">
+    <h1 className="text-3xl font-bold mb-5">Sobre Nós</h1>
+    <p className="mb-5 text-zinc-500 dark:text-zinc-400">
       Somos uma Assistência Técnica Especializada autorizada que trabalha
       com equipamentos dentro e fora da garantia. Com mais de 30 anos de
       experiência no mercado, nossa equipe altamente qualificada está
@@ -28,19 +29,20 @@ export default function Sobrenos() {
       equipamentos de informática.
     </p>
 
-    <h2 className="mb-8 text-xl font-bold" >Nossa Missão</h2>
-    <p>
+    <h2 className="mb-5 text-xl font-bold" >Nossa Missão</h2>
+    <p className="text-zinc-500 dark:text-zinc-400">
       Fornecer serviços de assistência técnica de alta qualidade, com
       transparência, ética e honestidade, garantindo a satisfação total de
       nossos clientes.
     </p>
 
-    <h3 className="mt-8 text-xl font-bold">Nossos Valores</h3>
-    <ul className="list-disc ml-6">
-      <li>Transparência em todos os processos</li>
-      <li>Compromisso com a qualidade</li>
-      <li>Respeito ao cliente</li>
-      <li>Ética profissional</li>
+    <h3 className="mt-5 text-xl font-bold">Nossos Valores</h3>
+    <ul className="list-disc ml-6 text-zinc-500 dark:text-zinc-400">
+    
+    <span><CircleCheck/>Transparência em todos os processos</span>
+    <CircleCheck/>Compromisso com a qualidade
+    <CircleCheck/>Respeito ao cliente
+    <CircleCheck/>Ética profissional
     </ul>
   </div>
 </section>
