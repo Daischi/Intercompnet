2faa34526f933509be14af164d320c6b4abc8683
Guilherme Poppi <guilhermepoppilm@gmail.com>
2025-03-11 19:32:12 -0300

    ajustando contatos para telas menores

--- src/components/Contatos/index.tsx
+++ src/components/Contatos/index.tsx
@@ -2,17 +2,19 @@ import { Clock, Mail, MapPin, Phone } from "lucide-react";
 
 export default function Contatos() {
   return (
-    <div>
+    <div id="contatos">
       <section>
         <div className="place-items-center">
-          <h1 className="text-3xl font-bold mb-2">Entre em Contato</h1>
+          <h1 className="text-2xl sm:text-3xl font-bold mb-2 text-center">
+            Entre em Contato
+          </h1>
           <div className="w-24 h-1 bg-gray-300 mx-auto mb-4"></div>
-          <p className="text-muted-foreground">
+          <p className="text-muted-foreground text-center px-4">
             Estamos prontos para atender suas necessidades
           </p>
         </div>
-        <section className="flex justify-center items-center mt-5">
-          <section className="border grid gap-5 py-9 rounded-lg px-5 scale-95">
+        <section className="flex flex-col md:flex-row justify-center items-center mt-5 px-4">
+          <section className="border grid gap-5 py-9 rounded-lg px-5 scale-95 w-full md:w-auto mb-6 md:mb-0">
             <section>
               <div className="flex items-center gap-4">
                 <div className="flex gap-3">
@@ -74,11 +76,10 @@ export default function Contatos() {
             </section>
           </section>
 
-          <section className="scale-95 border p-6 rounded-lg">
+          <section className="scale-95 border p-6 rounded-lg w-full md:w-auto">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14622.562629753636!2d-46.66964300000001!3d-23.617217!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5a0ab1500fa9%3A0x173ecfcb7cf74296!2sR.%20Ot%C3%A1vio%20Tarqu%C3%ADnio%20de%20Sousa%2C%20945%20-%20Campo%20Belo%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2004613-002!5e0!3m2!1sen!2sbr!4v1741728493764!5m2!1sen!2sbr"
-              width="400"
-              height="283"
+              className="w-full md:w-[400px] h-[283px]"
               loading="lazy"
             ></iframe>
           </section>
